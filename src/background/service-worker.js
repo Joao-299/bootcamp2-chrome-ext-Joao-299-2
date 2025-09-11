d437e48c06f6c7a9954d098abb764516c644b4b6
Joao-299 <joaovitornoronha24@outlook.com>
2025-09-11 17:39:59 -0300

    feat: Melhora a UX da primeira anotação.

--- src/background/service-worker.js
+++ src/background/service-worker.js
@@ -1,9 +1,4 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log('Extensão Quick Note instalada com sucesso!');
-  // Você pode definir um valor inicial aqui se quiser
-  chrome.storage.sync.get(['quickNote'], (result) => {
-    if (result.quickNote === undefined) {
-      chrome.storage.sync.set({ quickNote: 'Sua primeira anotação!' });
-    }
-  });
+  // Não vamos mais criar uma nota inicial. O storage começará vazio.
 });
